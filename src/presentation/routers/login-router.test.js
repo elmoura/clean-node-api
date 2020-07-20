2889618d05ad4623743d60a79c9af7ebaa65f7fd
Gabriel Moura <gabriel.brito@linkapi.com.br>
2020-07-20 08:15:07 -0300

    added invalid request tests

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,8 +1,38 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
-      return { statusCode: 400 }
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.serverError()
     }
+
+    const { email, password } = httpRequest.body
+
+    if (!email) {
+      return HttpResponse.badRequest('email')
+    }
+
+    if (!password) {
+      return HttpResponse.badRequest('password')
+    }
+  }
+}
+
+class HttpResponse {
+  static badRequest (paramName) {
+    return {
+      body: new MissingParamError(paramName),
+      statusCode: 400
+    }
+  }
+
+  static serverError () {
+    return { statusCode: 500 }
+  }
+}
+
+class MissingParamError extends Error {
+  constructor (paramName) {
+    super(`Missing param ${paramName}`)
+    this.name = 'MissingParamError'
   }
 }
 
@@ -19,5 +49,39 @@ describe('Login Router', () => {
     const httpResponse = SYSTEM_UNDER_TEST.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const SYSTEM_UNDER_TEST = new LoginRouter()
+
+    const httpRequest = {
+      body: {
+        email: 'elmoura@mail.com'
+      }
+    }
+
+    const httpResponse = SYSTEM_UNDER_TEST.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('Should return 500 if no httpRequest is provided', () => {
+    const SYSTEM_UNDER_TEST = new LoginRouter()
+
+    const httpResponse = SYSTEM_UNDER_TEST.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest has no body', () => {
+    const SYSTEM_UNDER_TEST = new LoginRouter()
+
+    const httpRequest = {}
+
+    const httpResponse = SYSTEM_UNDER_TEST.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
