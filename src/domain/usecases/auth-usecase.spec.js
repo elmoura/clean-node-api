29c1c212d7102078768b6d639f2d2b9634a01e46
Gabriel Moura <gabriel.brito@linkapi.com.br>
2021-01-03 13:17:01 -0300

    ensure AuthUseCase throws MissingParamError if no password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -5,6 +5,10 @@ class AuthUseCase {
     if (!email) {
       throw new MissingParamError('email')
     }
+
+    if (!password) {
+      throw new MissingParamError('password')
+    }
   }
 }
 
@@ -15,5 +19,13 @@ describe('Auth UseCase', () => {
     const accessTokenPromise = systemUnderTest.auth()
 
     expect(accessTokenPromise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should throw if no password is provided', async () => {
+    const systemUnderTest = new AuthUseCase()
+
+    const accessTokenPromise = systemUnderTest.auth('any_email@mail.com')
+
+    expect(accessTokenPromise).rejects.toThrow(new MissingParamError('password'))
   })
 })
